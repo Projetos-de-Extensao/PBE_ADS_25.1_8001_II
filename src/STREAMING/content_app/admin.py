8eeeae316f774c7c22cd385aecfd610f4a62ec86
GabrielPerrota <gabrielperrota2003@gmail.com>
2025-05-23 17:51:33 -0300

    Atualização Streaming

--- src/STREAMING/content_app/admin.py
+++ src/STREAMING/content_app/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
+from content_app.models import Content, Playlist
 
-from content_app import models
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content_type', 'is_public')
+    list_filter = ('content_type', 'is_public')
+    ordering = ('-upload_date')
+    search_fields = ('title', 'description')
+    fields = ('title', 'description', 'file_url', 'thumbnail_url', 'content_type', 'upload_date', 'views', 'likes', 'is_public', 'status')
+    fieldsets = (
+    ('Informações Básicas', {'fields': ('title', 'description')}),
+    ('Detalhes do Arquivo', {'fields': ('file_url', 'thumbnail_url')}),
+  )
 
-admin.site.register(models.Content)
-
+admin.site.register(Content)
+admin.site.register(Playlist)
