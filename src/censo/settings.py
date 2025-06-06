d50e1211611e2f3afb423b00f6cfd06cc3ae623e
GabrielPerrota <gabrielperrota2003@gmail.com>
2025-06-06 16:28:04 -0300

    Swagger em funcionamento

--- src/censo/settings.py
+++ src/censo/settings.py
@@ -40,7 +40,10 @@ INSTALLED_APPS = [
     'censoApp',  
     'rest_framework',
     'drf_yasg', 
+    'corsheaders',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -50,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'censo.urls'
